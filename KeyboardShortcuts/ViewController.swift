71a55cc61a20634877c1aeb385b286ecd8c874e2
Ernesto Valdez <ernesto.valdez@soportegrupocant.com>
2019-09-04 10:29:54 -0500

    added basic UI interface

--- KeyboardShortcuts/ViewController.swift
+++ KeyboardShortcuts/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblAllKeys: UILabel!
+    @IBOutlet weak var txtShortcutName: UITextField!
+    @IBOutlet weak var txtKeys:UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func onBtnAddKeyClick(_ sender: UIButton, forEvent event: UIEvent) {
+        
+        lblAllKeys.text = lblAllKeys.text! + txtKeys.text! + " ";
+        txtKeys.text = "";
+        
+    }
+    
+    @IBAction func onBtnSaveShortcutClicked(_ sender: UIButton, forEvent event: UIEvent) {
+        
+        txtShortcutName.text = "";
+        lblAllKeys.text = "";
+    }
+    
+    
+    
 }
 
